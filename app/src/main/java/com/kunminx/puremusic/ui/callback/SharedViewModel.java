50fcc145558654d73e5918eb926c0c197d04c157
KunMinX <kunminx@gmail.com>
2020-07-16 19:19:00 +0800

    remove useless code

--- app/src/main/java/com/kunminx/puremusic/ui/callback/SharedViewModel.java
+++ app/src/main/java/com/kunminx/puremusic/ui/callback/SharedViewModel.java
@@ -27,12 +27,4 @@ import com.kunminx.puremusic.data.bean.Moment;
 public class SharedViewModel extends ViewModel {
 
     public final UnPeekLiveData<Moment> moment = new UnPeekLiveData<>();
-
-    public UnPeekLiveData<Moment> test =
-            new UnPeekLiveData.Builder<Moment>()
-                    .setAllowNullValue(false)
-                    .setAllowToClear(true)
-                    .setEventSurvivalTime(500)
-                    .create();
-
 }
