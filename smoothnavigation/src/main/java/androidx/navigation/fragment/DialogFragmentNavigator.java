eae5e68e551fcebedea499b42fdb324f5a8767bb
KunMinX <kunminx@gmail.com>
2020-07-18 11:38:55 +0800

    exchange navigation pkg

--- smoothnavigation/src/main/java/androidx/navigation/fragment/DialogFragmentNavigator.java
+++ smoothnavigation/src/main/java/androidx/navigation/fragment/DialogFragmentNavigator.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.androidx.navigation.fragment;
+package androidx.navigation.fragment;
 
 import android.content.Context;
 import android.content.res.TypedArray;
