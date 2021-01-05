57272cfa7835881723269905b48ac403c039112b
kunminx <kunminx@gmail.com>
2021-01-05 20:40:32 +0800

    add child fragment sample in detail page

---
+++ app/src/main/java/com/kunminx/puremusic/ui/ChildCFragment.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2018-2020 KunMinX
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.kunminx.puremusic.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.kunminx.puremusic.R;
+import com.kunminx.puremusic.databinding.FragmentChildCBinding;
+import com.kunminx.puremusic.ui.base.BaseFragment;
+import com.kunminx.puremusic.ui.callback.SharedViewModel;
+import com.kunminx.puremusic.ui.state.DetailViewModel;
+
+/**
+ * Create by KunMinX at 2020/5/30
+ */
+public class ChildCFragment extends BaseFragment {
+
+    private DetailViewModel mDetailViewModel;
+    private SharedViewModel mSharedViewModel;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mDetailViewModel = getFragmentViewModel(DetailViewModel.class);
+        mSharedViewModel = getActivityViewModel(SharedViewModel.class);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_child_c, container, false);
+        FragmentChildCBinding binding = FragmentChildCBinding.bind(view);
+        binding.setLifecycleOwner(this);
+        binding.setClick(new ClickProxy());
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+    public class ClickProxy {
+
+        public void jump() {
+
+        }
+
+        public void back() {
+            nav().navigateUp();
+        }
+
+    }
+}
